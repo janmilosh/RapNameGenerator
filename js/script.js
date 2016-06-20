130436d042f2179bd6567f0e5901f108fdd1f4fd
janmilosh <janmilosh@gmail.com>
2016-06-20 07:04:59 -0400

    Working version

--- js/script.js
+++ js/script.js
@@ -14,19 +14,61 @@
 
 function Generator() {
 
-    /* Name Arrays: Customize names to change possible output */
-    this.last_names = ['the Chef', 'Digital', 'Wise', 'Knight', 'Wrecka', 'the Genius', 'the Zoo Keeper', 'the Monk', 'the Scientist', 'the Disciple', 'the Darkman', 'Pellegrino', 'the Ill Figure', 'Rocks The World', 'the Baptist',];
-    this.first_names = ['Inspectah', 'Masta', 'Poppa', 'Five Foot', 'Ghostface', 'Old Dirty'];
+  /* Name Arrays: Customize names to change possible output */
+  this.last_names = ['the Chef', 'Digital', 'Wise', 'Knight', 'Wrecka', 'the Genius', 'the Zoo Keeper', 'the Monk', 'the Scientist', 'the Disciple', 'the Darkman', 'Pellegrino', 'the Ill Figure', 'Rocks The World', 'the Baptist',];
+  this.first_names = ['Inspectah', 'Masta', 'Poppa', 'Five Foot', 'Ghostface', 'Old Dirty'];
 
 }
 
+function randomIndex(arrayLength) {
+  return Math.floor((Math.random() * arrayLength));
+}
 
-//Add your codez here
+function firstLetter(name) {
+  return name[0].toUpperCase();
+}
+
+function capsAndDots(name) {
+  return name.toUpperCase().split('').join('.');
+}
+
+function changedUserName(name) {
+  var userNameArray = [name, firstLetter(name), capsAndDots(name)];
+  return userNameArray[randomIndex(userNameArray.length)];
+}
+
+function rapName(userName, firstName, lastName) {
+  var options = [];
+  var newUserName = changedUserName(userName);
+  options[0] = [newUserName, lastName].join(' ');
+  options[1] = [firstName, newUserName].join(' ');
+  options[2] = [firstName, newUserName, lastName].join(' ');
+  return options[randomIndex(options.length)]
+}
 
 
 $(document).ready(function() {
+  var engine = new Generator;
+  var button = $('#enter');
+  
+  button.on('click', function() {
+    var firstName = engine.first_names[randomIndex(engine.first_names.length)];
+    var lastName = engine.last_names[randomIndex(engine.last_names.length)];
+    var userName = $('#user-input').val();
+    
+    if(userName.length > 0) {
+      
+      $('.error').hide();
+      $('.response').show();
+      
+      var displayName = rapName(userName, firstName, lastName);
+      $('.alert-success.response').text(displayName);
+    
+    } else {
 
-    var engine = new Generator;
-    //Add your codez here
+      $('.response').hide();
+      $('.error').show();
 
+    }
+  });
 });
